ffd869c354d0e1198384815cb315d32683dcffdc
Bee <pizzaman8099@gmail.com>
2020-02-08 15:57:32 -0800

    Fix docker and add experimental files

--- packages/server/server/server.js
+++ packages/server/server/server.js
@@ -49,12 +49,12 @@ app.use(
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
   app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', '..', 'packages', 'client', 'build', 'index.html'));
   })
 } else {
   app.use(express.static(path.join(__dirname, '../client/public')));
   app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'client', 'pubilc', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', '..', 'packages', 'client', 'public', 'index.html'));
   })
 }
 
